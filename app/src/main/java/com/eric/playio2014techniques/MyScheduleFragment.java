138eba3d90ad7f77ab9cbe1d9bc21e36e4c9df20
Eric Liu <eric.liu.developer@gmail.com>
2014-10-08 17:15:47 +0800

    Custom layout for listFragment

--- app/src/main/java/com/eric/playio2014techniques/MyScheduleFragment.java
+++ app/src/main/java/com/eric/playio2014techniques/MyScheduleFragment.java
@@ -3,12 +3,16 @@ package com.eric.playio2014techniques;
 import android.app.Activity;
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by HQ on 2014/10/8.
  */
 public class MyScheduleFragment extends ListFragment {
+
+    private View mRoot = null;
 
 
     public interface Listener {
@@ -17,6 +21,14 @@ public class MyScheduleFragment extends ListFragment {
         public void onFragmentAttached(MyScheduleFragment fragment);
 
         public void onFragmentDetached(MyScheduleFragment fragment);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRoot = inflater.inflate(R.layout.fragment_my_schedule, container, false);
+
+        return mRoot;
     }
 
     @Override
